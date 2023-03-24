4695f69b4082338a35d355ffcf18a630baa91968
abtahib <abtahib@pascal.cas.mcmaster.ca>
2023-03-24 04:23:55 -0400

    test

---
+++ L08/part4/worst.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+	int arr[2] = {1,2};
+	printf(arr[999]);
+
+	bool b = true;
+	b = b + 1;
+	
+	if (true == true) {
+		b++
+	} else if (true == true) {
+		b++
+	}
+
+	int k = 9;
+	k = k/0;
+
+
+}
+
